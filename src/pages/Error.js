10e2953d9eacb5c8090b09efe9a63afca40f6169
D. McDonald <d.mcdonald@MACs-MBP.attlocal.net>
2022-08-11 17:59:04 -0500

    netlify error  update

--- src/pages/Error.js
+++ src/pages/Error.js
@@ -1,11 +1,15 @@
+import { useEffect } from 'react'
 import { Container } from 'react-bootstrap'
 import Header from '../components/Header'
 
 export default function Error() {
-  return (
-    <>
-    <Header heading={"404 Error"} subHeading={"Seems to be an error. Head back to the home page"} hidden={'d-none'} />
-    <Container>Sorry...</Container>
-    </>
-  )
+
+  useEffect(() => {
+    return (
+      <>
+      <Header heading={"404 Error"} subHeading={"Seems to be an error. Head back to the home page"} hidden={'d-none'} />
+      <Container>Sorry...</Container>
+      </>
+    )
+  }, 5000)
 }
