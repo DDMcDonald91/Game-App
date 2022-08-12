42b9eb20883834d7e8ab22043b9de1e9d237526d
D. McDonald <d.mcdonald@MACs-MBP.attlocal.net>
2022-08-12 11:01:14 -0500

    second footer update

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,8 +3,10 @@ import { Container } from 'react-bootstrap'
 export default function Footer() {
   return (
     <Container id='footer' fluid>
+        <Container>
         <p style={{color: 'white', paddingTop: '6rem'}} className="text-center mt-4 mb-4">Designed & Created by DMDesigns.</p>
-        <p style={{color: 'white', fontSize: '.9rem'}} className="text-center mt-4 mb-4">© 2022 DMDesigns, all rights reserved. All trademarks are property of their respective owners.</p> 
+        <p style={{color: 'white', fontSize: '.9rem'}} className="text-center mt-4 mb-4">© 2022 DMDesigns, all rights reserved. All trademarks are property of their respective owners.</p>
+        </Container> 
     </Container>
   )
 }
